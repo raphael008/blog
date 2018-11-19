50a74e4088258406c4e86043058077728ad6c484
raphael008 <raphael008@outlook.com>
2018-11-19 13:40:45 +0800

    测试Jackson消息转换

---
+++ blog-model/src/main/java/com/github/raphael008/model/Test.java
@@ -0,0 +1,12 @@
+package com.github.raphael008.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Test {
+    String username;
+    String password;
+}
--- blog-web/src/main/java/com/github/raphael008/controller/TestController.java
+++ blog-web/src/main/java/com/github/raphael008/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.github.raphael008.controller;
 
+import com.github.raphael008.model.Test;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("hello")
-    public String test() {
-        return "Hello, world";
+    public Test test() {
+        Test test = new Test();
+        test.setUsername("raphael008");
+        test.setPassword("abc1234");
+        return test;
     }
 }
