7ac45ec64a4f5181117423e376d2a8bebbc702c2
raphael008 <raphael008@icloud.com>
2018-11-25 22:03:15 +0800

    使用Redis管理Session

--- blog-web/src/main/java/com/github/raphael008/controller/WebController.java
+++ blog-web/src/main/java/com/github/raphael008/controller/WebController.java
@@ -3,6 +3,8 @@ package com.github.raphael008.controller;
 import com.github.raphael008.auth.manager.LoginManager;
 import com.github.raphael008.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.session.FindByIndexNameSessionRepository;
+import org.springframework.session.Session;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +20,9 @@ public class WebController {
     @Autowired
     private LoginManager loginManager;
 
+    @Autowired
+    FindByIndexNameSessionRepository sessionRepository;
+
     @PostMapping("login")
     public User login(HttpServletRequest request, @RequestBody Map<String, String> params) {
         try {
@@ -26,5 +31,12 @@ public class WebController {
         } catch (Exception e) {
             throw new RuntimeException("登录失败。");
         }
+    }
+
+    @PostMapping("session")
+    public void session() {
+        Map test = sessionRepository.findByIndexNameAndIndexValue(FindByIndexNameSessionRepository.PRINCIPAL_NAME_INDEX_NAME, "test");
+        Session a = sessionRepository.findById("a");
+        System.out.println();
     }
 }
